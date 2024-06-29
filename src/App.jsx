adca115b3a3b074ab20555beb52f24fd0f9010e9
networkdeji <ayodejiabu7@gmail.com>
2024-06-29 15:29:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from './Header'
+import Meme from "./Meme"
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <Header />
+      <Meme />
+    </>
+  )
+}
+
+export default App
